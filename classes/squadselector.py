3b1fda441b3eab8d0232af92a15e4048d106c7d3
Tim Mitchell <tim_mitchell@outlook.com>
2018-05-20 22:43:25 +0100

    updated squadselector.py using treeview. more to do but loads

--- classes/squadselector.py
+++ classes/squadselector.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+import operator
+from tkinter import ttk
 
 class PickSquad(tk.Frame):
     def __init__(self, master, club):
@@ -23,23 +25,76 @@ class PickSquad(tk.Frame):
         club = club[0]
 
                 # Set the window size
-        classes.game.Game.set_window_size(master, "500x600")
+        classes.game.Game.set_window_size(master, "600x500")
 
-        # Create the scrollbar
-        scrollbar = tk.Scrollbar(self)
-        scrollbar.pack( side = 'right', fill = 'y' )
-
-        # Get a list of eligible players for the club
-        player_name_list = tk.Listbox(self, yscrollcommand = scrollbar.set )
+        player_tree = ttk.Treeview(self)
+        player_tree["columns"] = ('movement', 'attack', 'defense', 'strength', 'toughness', 'resilience', 'throwing', 'kicking', 'agility', 'catching')
+        player_tree.heading('#0', text="Name")
+        player_tree.column("movement", width=35)
+        player_tree.heading("movement", text="MOV")
+        player_tree.column("attack", width=35)
+        player_tree.heading("attack", text="ATT")
+        player_tree.column("defense", width=35)
+        player_tree.heading("defense", text="DEF")
+        player_tree.column("strength", width=35)
+        player_tree.heading("strength", text="STR")
+        player_tree.column("toughness", width=35)
+        player_tree.heading("toughness", text="TOU")
+        player_tree.column("resilience", width=35)
+        player_tree.heading("resilience", text="RES")
+        player_tree.column("throwing", width=35)
+        player_tree.heading("throwing", text="THR")
+        player_tree.column("kicking", width=35)
+        player_tree.heading("kicking", text="KIC")
+        player_tree.column("agility", width=35)
+        player_tree.heading("agility", text="AGI")
+        player_tree.column("catching", width=35)
+        player_tree.heading("catching", text="CAT")
         for player in club.players:
             if player.availability == 'available':
-                player_name_list.insert('end', player.name)
+                #player_tree.insert(' ', 'end', text=player.name, values=(player.movement, player.attack, player.defense, player.strength, player.toughness, player.resilience, player.throwing, player.kicking, player.agility, player.catching))
+                player_tree.insert("", 'end', text=player.name, values=(4, 3, 3, 4, 3, 7, 8, 9, 7, 10))
 
-        player_name_list.pack( side = 'left', fill = 'both' )
-        scrollbar.config( command = player_name_list.yview )
+        player_tree.pack()
+        '''
+        # Create the scrollbar
+        self.scrollbar = tk.Scrollbar(self)
+        self.scrollbar.pack( side = 'right', fill = 'y' )
+
+        # Get a list of eligible players for the club
+        self.player_name_list = tk.Listbox(self, yscrollcommand = self.scrollbar.set )
+        #self.player_name_list = tk.Listbox(self, yscrollcommand = self.yscroll1 )
+        #self.player_movement_list = tk.Listbox(self, yscrollcommand = self.yscroll2 )
+        for player in club.players:
+            if player.availability == 'available':
+                player_stats = operator.attrgetter('movement', 'attack', 'defense', 'strength', 'toughness', 'resilience', 'throwing', 'kicking', 'agility', 'catching')(player)
+                # insert delimiter between the stats for readability
+                stats_string = ' | '.join(str(s) for s in player_stats)
+                self.player_name_list.insert('end', stats_string)
+                #self.player_movement_list.insert('end', player.movement)
+
+        self.player_name_list.pack( side = 'left', fill = 'both' )
+        #self.player_movement_list.pack( side = 'left', fill = 'both' )
+        self.scrollbar.config( command = self.player_name_list.yview )
+        #self.scrollbar.config( command = self.yview )
 
         #ok_button = tk.Button(self, highlightbackground='#9E332C', text="Continue", pady=20, command=lambda : classes.game.Game.restart_game(config.game))
+        '''
+    '''
+    def yscroll1(self,*args):
+        if self.player_movement_list.yview() != self.player_name_list.yview():
+            self.player_movement_list.yview_moveto(args[0])
+        self.scrollbar.set(*args)
 
+    def yscroll2(self,*args):
+        if self.player_name_list.yview() != self.player_movement_list.yview():
+            self.player_name_list.yview_moveto(args[0])
+        self.scrollbar.set(*args)
+
+    def yview(self, *args):
+        self.player_name_list.yview(*args)
+        self.player_movement_list.yview(*args)
+    '''
         
 import classes.game
 
